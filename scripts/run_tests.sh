64d8141210b34eed7545b8c5d3530462625f7e28
timerke <timerke@mail.ru>
2023-11-15 14:31:35 +0300

    Fixed test script

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,10 +1,4 @@
 cd ..
-rm -rf venv
-python3 -m venv venv
-./venv/bin/python3 -m pip install --upgrade pip
-./venv/bin/python3 -m pip install -r requirements.txt
-./venv/bin/python3 -m pip install flake8
-
 echo "--- Run all tests ---"
 ./venv/bin/python3 -m unittest discover tests -v
 echo "--- Check flake8 ---"
