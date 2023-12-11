4bd486cccf6224c3b1d2750162901239a4f8df65
timerke <timerke@mail.ru>
2023-12-11 11:35:20 +0300

    Changed test scripts

--- scripts/run_tests.bat
+++ scripts/run_tests.bat
@@ -1,7 +1,9 @@
+echo off
 cd ..
 echo --- Run all tests ---
 venv\Scripts\python -m unittest discover tests -v
 echo --- Check flake8 ---
+venv\Scripts\python -m pip install flake8
 venv\Scripts\python -m flake8 .
 echo --- Done ---
 pause
--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -2,5 +2,6 @@ cd ..
 echo "--- Run all tests ---"
 ./venv/bin/python3 -m unittest discover tests -v
 echo "--- Check flake8 ---"
+./venv/bin/python3 -m pip install flake8
 ./venv/bin/python3 -m flake8 .
 echo "--- Done ---"
